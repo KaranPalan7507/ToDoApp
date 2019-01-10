59273e1955ef0a5ce4cea040a8a4c2e5d02ee2ea
Karan Palan <karanpalan7507@gmail.com>
2019-01-10 23:21:36 +0530

    Updates

---
+++ precache-manifest.f84dd5e05959aab6ff65ad48722361d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ef6ea77c30c55a5d0cf",
+    "url": "/ToDoApp/static/js/runtime~main.6ef6ea77.js"
+  },
+  {
+    "revision": "f1eae6f6bba3e92c53d9",
+    "url": "/ToDoApp/static/js/main.f1eae6f6.chunk.js"
+  },
+  {
+    "revision": "b07c18fb1abb3c1e78e2",
+    "url": "/ToDoApp/static/js/1.b07c18fb.chunk.js"
+  },
+  {
+    "revision": "f1eae6f6bba3e92c53d9",
+    "url": "/ToDoApp/static/css/main.3e3ef61e.chunk.css"
+  },
+  {
+    "revision": "8b444a2030766603777dfb10eb7ce6ab",
+    "url": "/ToDoApp/index.html"
+  }
+];
